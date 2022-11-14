92a9d6d4d02aef8068cbb54f34fe8e7336fe3fc0
Siddhant Jain <siddhant.21910811@viit.ac.in>
2022-11-14 18:00:00 +0530

    refresh token

--- src/main/java/com/mavs/backend/controllers/admin/AdminController.java
+++ src/main/java/com/mavs/backend/controllers/admin/AdminController.java
@@ -1,14 +1,25 @@
 package com.mavs.backend.controllers.admin;
 
+import java.util.Map.Entry;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mavs.backend.entities.JwtResponse;
+import com.mavs.backend.helper.JwtUtil;
 import com.mavs.backend.helper.ResponseMessage;
 import com.mavs.backend.services.admin.AdminService;
+
+import io.jsonwebtoken.impl.DefaultClaims;
 
 @RestController
 public class AdminController {
@@ -18,6 +29,12 @@ public class AdminController {
 
     @Autowired
     public AdminService adminService;
+
+    @Autowired
+    public JwtUtil jwtUtil;
+
+    @Autowired
+    public JwtResponse jwtResponse;
     
     @PostMapping("/register")
     public ResponseEntity<?> addAdmin(@RequestParam("email") String email,@RequestParam("password")String password,@RequestParam("name")String name){
@@ -41,5 +58,29 @@ public class AdminController {
             responseMessage.setMessage(e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseMessage);
         }
+    }
+
+    @GetMapping("/refresh-token")
+    public ResponseEntity<?> refreshToken(HttpServletRequest request) throws Exception{
+        // From the HttpRequest get the claims
+        DefaultClaims claims = (io.jsonwebtoken.impl.DefaultClaims) request.getAttribute("claims");
+
+        Map<String,Object> expectedMap = getMapFromIoJsonwebtokenClaims(claims);
+
+        String token = jwtUtil.doGenerateRefreshToken(expectedMap, expectedMap.get("sub").toString()); 
+
+        jwtResponse.setMessage("Token refreshed successfully!!");
+        jwtResponse.setToken(token);
+        return ResponseEntity.ok(jwtResponse);
+
+
+    }
+
+    private Map<String, Object> getMapFromIoJsonwebtokenClaims(DefaultClaims claims) {
+        Map<String, Object> expectedMap = new HashMap<String, Object>();
+		for (Entry<String, Object> entry : claims.entrySet()) {
+			expectedMap.put(entry.getKey(), entry.getValue());
+		}
+		return expectedMap;
     }
 }
