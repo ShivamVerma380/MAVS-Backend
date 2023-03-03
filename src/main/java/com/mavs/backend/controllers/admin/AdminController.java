1e06997a8edbd3219064fc575a0372a7b8c11d66
Siddhant Jain <siddhant.21910811@viit.ac.in>
2023-03-03 11:44:30 +0530

    check

--- src/main/java/com/mavs/backend/controllers/admin/AdminController.java
+++ src/main/java/com/mavs/backend/controllers/admin/AdminController.java
@@ -3,6 +3,7 @@ package com.mavs.backend.controllers.admin;
 import java.util.Map.Entry;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -11,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -77,6 +79,8 @@ public class AdminController {
 
 
     }
+
+    
 
     private Map<String, Object> getMapFromIoJsonwebtokenClaims(DefaultClaims claims) {
         Map<String, Object> expectedMap = new HashMap<String, Object>();
